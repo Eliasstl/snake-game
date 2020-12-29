e836613fa8a6ef404a605e6c526f065093341354
unknown <ejmotoboy1@gmail.com>
2020-12-29 00:30:45 -0200

    Criando comida da cobrinha

--- script.js
+++ script.js
@@ -9,9 +9,14 @@ snake[0]={
 	y:8 * box
 }
 let direction = "right";
+let food ={
+	x: Math.floor(Math.random() * 15 + 1)*box,
+	y: Math.floor(Math.random() * 15 + 1)*box
+}
 function criarBG(){
 	context.fillStyle="lightgreen";
 	context.fillRect(0,0,16*box,16*box);
+	
 }
 function criarCobrinha(){
 	for(i=0; i< snake.length;i++){
@@ -19,7 +24,10 @@ function criarCobrinha(){
 		context.fillRect(snake[i].x, snake[i].y, box, box);
 	}
 }
-
+function drawFood(){
+	context.fillStyle = "red";
+	context.fillRect(food.x, food.y, box, box);
+}
 document.addEventListener('keydown',update);
 
 function update(event){
@@ -34,9 +42,10 @@ function iniciarJogo(){
 	if(snake[0].x < 0 && direction == "left") snake[0].x =16*box;
 	if(snake[0].y > 15 * box && direction == "down") snake[0].y =0;
 	if(snake[0].y < 0 && direction =="up") snake[0].y = 16 * box;
-	
+
 	criarBG();
     criarCobrinha();
+	drawFood();
 	let snakeX= snake[0].x;
 	let snakeY= snake[0].y;
 	
